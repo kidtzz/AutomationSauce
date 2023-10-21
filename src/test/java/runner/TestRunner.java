af36ad103822d38752d224c3a7393b86664bb2b8
kidtz <bangkitpr4@gmail.com>
2023-10-21 21:04:48 +0700

    [modify] add driver

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,13 +1,30 @@
 package runner;
 
-import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
-@RunWith(Cucumber.class)
-@CucumberOption(
-        feutures = "src/java/features",
-        glue = {"src/test/java/stepDefinition", "src/test/java/utility"},
-        plugin = {"pretty", "html:target/cucumber-html-report","json:cucumber.json"}
-)
-public final class TestRunner {
+public class TestRunner {
 
+    @Test
+    void clickAndHold() throws InterruptedException {
+        System.setProperty("webdriver.firefox.driver" ,
+                System.getProperty("user.dir") + "src/test/resources/drivers/geckodriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        WebElement titleC = driver.findElement(By.xpath("//li[text()= 'C']"));
+        WebElement titleA = driver.findElement(By.xpath("//li[text()= 'A']"));
+        Actions actions = new Actions(driver);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", titleC);
+        actions.moveToElement(titleA).build().perform();
+        actions.clickAndHold().build().perform();
+        Thread.sleep(5000);
+        driver.close();
+    }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,8 +1,7 @@
-Feature: Login Website SauceDemo
-
-  Scenario: Login Saouce
-    Given Input username
-    Given input Password
-    When Klik button Login
-    Then Login Success
-
+Scenario: login with valid credentials
+Given I am on the login page
+When I fill in "Email" with "test@test.com"
+And I fill in "Password" with "test123pass"
+And I press "Login"
+Then I should be on the users home page
+And I should see "Login successful"
